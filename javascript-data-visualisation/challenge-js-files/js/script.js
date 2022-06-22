0694a6d560d9b86ef2dc2770481906e9a56339f9
john4e656f <neo@kali.Neo>
2022-06-22 12:53:17 +0200

    second solution

--- javascript-data-visualisation/challenge-js-files/js/script.js
+++ javascript-data-visualisation/challenge-js-files/js/script.js
@@ -1,5 +1,3 @@
-console.log(document.body)
-
 
 //Gather Data from the DOM
 
@@ -80,31 +78,58 @@ const myChart = new Chart(ctx, {
     }
 });
 
+let dataObj = {}
+let dataCountry=[];
+let data2 = [];
+let data3 = [];
+let data4 = [];
+let data5 = [];
+let data6 = [];
+let data7 = [];
+let data8 = [];
+let data9 = [];
+let data10 = [];
+let data11 = [];
+let data12 = [];
+
+let x = [];
 function gatherDomData(e) {
-    let data =[];
+    
     /* Getting the number of rows in the table. */
     let rowLength = e.rows.length;
-    console.log(rowLength);
+    // console.log(rowLength);
 
-    for (let i = 0; i < rowLength; i++) {
-        /* Getting the rows of the table. */
-        let row = e.rows[i];
-        console.log(row);
+    for (let i = 1; i < rowLength; i++) {
         /* Getting the cells of the table. */
         let col = e.rows.item(i).cells;
-        // console.log(col);
-        /* Getting the number of columns in the table. */
-        let colLength = col.length;
-        // console.log(colLength);
-        for (let j = 0; j < colLength; j++) {
-            /* Getting the data of the table. */
-            let data1 = col.item(j).innerHTML;
-            console.log(data1);
-            data.push(data1);
-        }        
-       
+
+        dataCountry.push(col.item(1).innerText);
+        data2.push(col.item(2).innerHTML);
+        data3.push(col.item(3).innerHTML);
+        data4.push(col.item(4).innerHTML);
+        data5.push(col.item(5).innerHTML);
+        data6.push(col.item(6).innerHTML);
+        data7.push(col.item(7).innerHTML);
+        data8.push(col.item(8).innerHTML);
+        data9.push(col.item(9).innerHTML);
+        data10.push(col.item(10).innerHTML);
+        data11.push(col.item(11).innerHTML);
+        data12.push(col.item(12).innerHTML);
+        
     }
-    return data;
+    console.log(x);
 }
 
+console.log(dataCountry);
+console.log(data2);
+console.log(data3);
+console.log(data4);
+console.log(data5);
+console.log(data6);
+console.log(data7);
+console.log(data8);
+console.log(data9);
+console.log(data10);
+console.log(data11);
+console.log(data12);
 gatherDomData(table1);
